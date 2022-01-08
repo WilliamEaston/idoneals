15a000c913f044e695ffc19de3cf240c0a30b5ec
WilliamEaston <31230116+WilliamEaston@users.noreply.github.com>
2022-01-08 12:52:54 -0800

    Removed dependency on generalized Riemann Hypothesis
    Also optimized isPrime and modPow, and added a method to get the needed witnesses rather than manually changing them.

--- IdonealSearch.java
+++ IdonealSearch.java
@@ -1,19 +1,24 @@
+import java.math.BigInteger;
 import java.text.NumberFormat;
 import java.util.ArrayList;
-import java.math.BigInteger;
+import java.util.Arrays;
 
-/* 
+/*
    This is a program to check the numbers up to a desired N for idoneality.
-   That is, whether or not they are one of Euler's idoneal numbers.
+   That is, whether they are one of Euler's idoneal numbers.
    So far, it has checked up to 100 billion.
-   
+
    Made by William Easton, December 2021 - January 2022
 */
 public class IdonealSearch {
 
     public static final long N = 100000000000L;  // the number to check up to
-    public static final double ln2 = Math.log(2);
 
+    public static final double ln2 = Math.log(2);
+    public static final double sqrtMaxLong = Math.sqrt(Long.MAX_VALUE);
+    public static long[] witnesses = getWitnesses();
+
+    // main w/ coverage checks
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
 
@@ -36,19 +41,20 @@ public class IdonealSearch {
                 }
             }
         }
+
         System.out.println();
-        System.out.println(numIdoneals + " idoneals found up to " + NumberFormat.getInstance().format(N) +
-                           " in " + (System.currentTimeMillis() - start) / 1000 + " seconds.");
+        System.out.println(numIdoneals + " idoneals found up to " + NumberFormat.getInstance().format(N)
+                + " in " + (System.currentTimeMillis() - start) / 1000.0 + " seconds, using "
+                + Arrays.toString(witnesses) + " for witnesses.");
     }
-    
+
     // Checks if n is idoneal using n + k^2 method
     public static boolean checkN(long n) {
         long maxK = (long)Math.sqrt(n * 3);
         boolean even = n % 2 == 0;
         for (long k = 1; k <= maxK; k += even ? 2 : 1)
-            if (gcd(k, n) == 1)
-                if (!meetsParams(n + k * k))
-                    return false;
+            if (gcd(k, n) == 1 && !meetsParams(n + k * k))
+                return false;
         return true;
     }
 
@@ -64,20 +70,20 @@ public class IdonealSearch {
     public static boolean isPrime(long n) {
         if (n == 2) return true;
         if (n % 2 == 0 || n == 1) return false;
-        // n = d*2^r + 1
-        long r = Long.numberOfTrailingZeros(n-1);
-        long d = (n - 1) / (long)Math.pow(2, r);
-        // Sufficient a's for n < 2,152,302,898,747
-        long[] aValues = new long[] {2, 3, 5, 7, 11};
-        long maxA = (long)Math.min(n-2, Math.floor(2*Math.pow(Math.log(n), 2)));
+
+        // write n as d*2^r + 1
+        long r = Long.numberOfTrailingZeros(n - 1);
+        long d = (n - 1) >> r;
+
         WitnessLoop:
-        for (long a : aValues) {
-            if (a > maxA) break;
-            long x = modPow(a, d, n);
+        for (long witness : witnesses) {
+            if (witness > n - 2)
+                break;
+            long x = modPow(witness, d, n);   // check b^d = 1 mod n
             if (x == 1 || x == n - 1)
                 continue WitnessLoop;
-            for (long i = 0; i < r - 1; i++) {
-                x = modPow(x, 2, n);
+            for (long i = 1; i < r ; i++) {
+                x = x < sqrtMaxLong ? (x * x) % n : modPow(x, 2, n);    // check b^(d*2^i) = -1 mod n
                 if (x == n - 1)
                     continue WitnessLoop;
             }
@@ -88,19 +94,19 @@ public class IdonealSearch {
 
     // Calculates base^power % mod for especially large numbers that would overflow longs
     public static long modPow(long base, long power, long mod) {
-        return new BigInteger(base + "").modPow(new BigInteger(power + ""), new BigInteger(mod + "")).longValue();
+        return BigInteger.valueOf(base).modPow(BigInteger.valueOf(power), BigInteger.valueOf(mod)).longValue();
     }
 
     // Returns the greatest common denominator of two numbers
     public static long gcd(long a, long b) {
         long max = 1;
-        for (long i : getFactors(a))
-            if (i > max && b % i == 0)
-                max = i;
+        for (long factor : getFactors(a))
+            if (factor > max && b % factor == 0)
+                max = factor;
         return max;
     }
 
-    // Returns all the factors except 1 of the input
+    // Returns all the factors of the input except 1
     public static ArrayList<Long> getFactors(long n) {
         ArrayList<Long> factors = new ArrayList<>();
         for (double i = 2; i <= Math.sqrt(n); i++) {
@@ -112,6 +118,22 @@ public class IdonealSearch {
         }
         factors.add(n);
         return factors;
+    }
+
+    // Returns the list of sufficient witnesses for all possible primes considering N
+    public static long[] getWitnesses() {
+        long[] primes = new long[] {2, 3, 5, 7, 11, 13, 17, 19};
+        long maxPrime = 4*N;
+        int numNeededPrimes;
+             if (maxPrime < 2047L)             numNeededPrimes = 1;
+        else if (maxPrime < 1373653L)          numNeededPrimes = 2;
+        else if (maxPrime < 25326001L)         numNeededPrimes = 3;
+        else if (maxPrime < 3215031751L)       numNeededPrimes = 4;
+        else if (maxPrime < 2152302898747L)    numNeededPrimes = 5;
+        else if (maxPrime < 3474749660383L)    numNeededPrimes = 6;
+        else if (maxPrime < 341550071728321L)  numNeededPrimes = 7;
+        else                                   numNeededPrimes = 8;
+        return Arrays.copyOf(primes, numNeededPrimes);
     }
 
     // Checks if n is an integer and prime
